1f9840606d1a777b04043d79d488c88eb384163e
r-rin <rafikov.rinat.m@gmail.com>
2024-03-31 14:50:18 +0300

    Small change in `sql_script.sql`

--- data/sql_script.sql
+++ data/sql_script.sql
@@ -9,6 +9,7 @@
     How to run:
     > mysql -u your_user -p < path/to/sql_script.sql
  */
+DROP DATABASE Supermarket;
 SET GLOBAL event_scheduler = ON;
 
 CREATE DATABASE IF NOT EXISTS Supermarket;
